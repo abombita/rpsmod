a611400833ca26a9ba9457990898f18d29226d6d
Astraea Bombita <abombita98@hotmail.com>
2021-06-30 04:30:24 -0400

    i think i did it

---
+++ index.js
@@ -0,0 +1,47 @@
+
+class rpsMod {
+
+    constructor(choice){
+
+        const options = ['rock','paper','scissors'];
+
+        let results;
+
+        if (!options.some(substring => choice.toLowerCase() == substring)) {
+            result = `{"status":"failure", "error":"Invalid choice provided"}`;
+        };
+
+        const computerChoice = options[Math.floor(Math.random()* options.length)];
+        const playerChoice = choice;
+
+        if (computerChoice == playerChoice) {
+            result = `{"status": "success", "computer": "${computerChoice}", "winner": "tie"}`;
+        }
+        else if (playerChoice == 'rock'){
+            if (computerChoice == 'paper'){
+                result = `{"status": "success", "computer": "${computerChoice}", "winner": "computer"}`;
+            } else {
+                result = `{"status": "success", "computer": "${computerChoice}", "winner": "player"}`;
+            }
+        }
+        else if (playerChoice == 'paper'){
+            if (computerChoice == 'scissors'){
+                result = `{"status": "success", "computer": "${computerChoice}", "winner": "computer}`;
+            } else {
+                result = `{"status": "success", "computer": "${computerChoice}", "winner": "player"}`;
+            }
+        }
+        else if (playerChoice == 'scissors'){
+            if (computerChoice == 'rock'){
+                result = `{"status": "success", "computer": "${computerChoice}", "winner": "computer"}`;
+            } else {
+                result = `{"status": "success", "computer": "${computerChoice}", "winner": "player"}`;
+            }
+        };
+
+        let finalResults = JSON.parse(result);
+        return finalResults;
+    };
+};
+
+module.exports = rpsMod;
